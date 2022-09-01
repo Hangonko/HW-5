8b5635261aa1773541e4f88ba5d71dccc03be446
Hangonko <okropiridzegio049@gmail.com>
2022-09-01 23:30:57 +0400

    .

--- homework-5/src/App.js
+++ homework-5/src/App.js
@@ -1,103 +1,144 @@
 import React, { useState, useEffect } from "react";
 import "./reset.css";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
-  const [sex, setSex] = useState("");
-
+  const initValues = {
+    userName: "",
+    lastName: "",
+    email: "",
+    age: "",
+    sex: "",
+  };
+  const [userValues, setUserValues] = useState(initValues);
+  const [errors, setErrors] = useState({});
+  const [isSubmited, setIsSubmited] = useState(false);
   const [users, setUsers] = useState([]);
 
   const id = new Date().valueOf();
 
-  const user = (preValue) => {
-    return [
-      ...preValue,
-      {
-        name: name,
-        lastName: lastName,
-        email: email,
-        age: age,
-        sex: sex,
-        id: id,
-      },
-    ];
+  useEffect(() => {
+    console.log(errors);
+    console.log(userValues);
+  }, [errors]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("http://localhost:3001/users");
+      setUsers(data.data);
+      console.log(data);
+    };
+    getData();
+  }, []);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setUserValues({ ...userValues, [name]: value });
+    console.log(userValues);
   };
 
-  const addUser = (e) => {
-    setUsers(user);
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
-    setName("");
-    setLastName("");
-    setEmail("");
-    setAge("");
-    setSex("");
+    setErrors(validate(userValues));
+    if (isSubmited) {
+      await axios.put(
+        `http://localhost:3001/users/${userValues.id}`,
+        userValues
+      );
+    } else {
+      await axios.post("http://localhost:3001/users/", userValues);
+    }
+    const { data } = await axios.get("http://localhost:3001/users/");
+    setUsers(data.data);
+    setUserValues(initValues);
+    setIsSubmited(false);
   };
-  console.log(users);
 
-  const deleteUser = (id) => {
-    setUsers((prev) => {
-      const filterUsers = prev.filter((user) => user.id !== id);
-      return filterUsers;
-    });
+  const validate = (values) => {
+    const errs = {};
+    if (values.userName && values.userName.length < 4) {
+      errs.userName = "სახელი უნდა შეიცავდეს მინუმუ 4 ასოს";
+    } else if (!values.userName) {
+      errs.userName = "შეიყვანეთ თქვენი სახელი";
+    }
+
+    if (values.lastName && values.lastName.length < 4) {
+      errs.lastName = "გვარი უნდა შეიცავდეს მინიმუ 4 ასოს";
+    } else if (!values.lastName) {
+      errs.lastName = "შეიყვანეთ თქვენი გვარი";
+    }
+
+    if (values.email && !values.email.includes("@gmail.com")) {
+      errs.email = "მეილი უნდა შეიცავდეს @gmail.com";
+    } else if (!values.email) {
+      errs.email = "შეიყვანეთ თქვენი მეილი";
+    }
+
+    if (values.age && !(values.age >= 18)) {
+      errs.age = "ასაკი უნდა აღემატებოდეს 18 წელს ";
+    } else if (!values.age) {
+      errs.age = "შეიყვანეთ თქვენი ასაკი";
+    }
+
+    return errs;
   };
 
-  const updateUser = (userID) => {
-    setUsers((prev) => {
-      // const findx = prev.findIndex((user) => user.userID === userID);
-      // return newArr;
-    });
-  };
+  // const deleteUser = (id) => {
+  //   setUsers((prev) => {
+  //     const filterUsers = prev.filter((user) => user.id !== id);
+  //     return filterUsers;
+  //   });
+  // };
+
+  // const updateUser = (userID) => {
+  //   setUsers((prev) => {
+  //     // const findx = prev.findIndex((user) => user.userID === userID);
+  //     // return newArr;
+  //   });
+  // };
 
   return (
     <>
-      <form action="submit" onSubmit={addUser}>
+      <form action="submit" onSubmit={handleOnSubmit}>
         <input
-          value={name}
-          placeholder="Name"
-          onChange={(e) => {
-            setName(e.target.value);
-            console.log(e.target.value);
-          }}
+          name="userName"
+          value={userValues.userName}
+          placeholder="Username"
+          onChange={handleOnChange}
           className="inputs"
         />
+        <p className="errMessage">{errors.userName}</p>
         <input
-          value={lastName}
+          name="lastName"
+          value={userValues.lastName}
           placeholder="LastName"
-          onChange={(e) => {
-            setLastName(e.target.value);
-            console.log(e.target.value);
-          }}
+          onChange={handleOnChange}
           className="inputs"
         />
+        <p className="errMessage">{errors.lastName}</p>
         <input
-          value={email}
+          name="email"
+          value={userValues.email}
           placeholder="Email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-            console.log(e.target.value);
-          }}
+          onChange={handleOnChange}
           className="inputs"
         />
+        <p className="errMessage">{errors.email}</p>
         <input
-          value={age}
+          name="age"
+          value={userValues.age}
           placeholder="Age"
-          onChange={(e) => {
-            setAge(e.target.value);
-            console.log(e.target.value);
-          }}
+          onChange={handleOnChange}
           className="inputs"
         />
+        <p className="errMessage">{errors.age}</p>
         <select
+          name="sex"
           defaultValue={"Choose"}
           className="select"
-          onChange={(e) => {
-            setSex(e.target.value);
-            console.log(e.target.value);
-          }}
+          value={userValues.sex}
+          onChange={handleOnChange}
         >
           <option value="Choose">Choose</option>
           <option value="male">Male</option>
@@ -107,29 +148,16 @@ const App = () => {
         <button className="button">Add</button>
       </form>
       {users.map((user) => {
-        const { name, lastName, email, age, sex, id } = user;
+        const { userName, lastName, email, age, sex } = user;
         return (
-          <div className="users" key={id}>
-            <p>{name}</p>
+          <div className="users">
+            <p>{userName}</p>
             <p>{lastName}</p>
             <p>{email}</p>
             <p>{age}</p>
             <p>{sex}</p>
-            <button
-              onClick={() => {
-                deleteUser(id);
-              }}
-            >
-              Remove
-            </button>
-            <button
-              onClick={() => {
-                updateUser(id);
-              }}
-            >
-              Edit
-            </button>
-            {console.log(id)}
+            <button>Remove</button>
+            <button>Edit</button>
           </div>
         );
       })}
